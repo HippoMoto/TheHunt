b94e4f2592f18cb74059cab71ba6d7a0cd10c3b6
Sojourner1066 <morgangrundy@gmail.com>
2026-03-04 21:56:59 +0000

    Initial Commit

---
+++ TheHunt/TheHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheHuntApp.swift
+//  TheHunt
+//
+//  Created by Morgan Grundy on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct TheHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheHuntTests/TheHuntTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheHuntTests.swift
+//  TheHuntTests
+//
+//  Created by Morgan Grundy on 3/4/26.
+//
+
+import Testing
+@testable import TheHunt
+
+struct TheHuntTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
